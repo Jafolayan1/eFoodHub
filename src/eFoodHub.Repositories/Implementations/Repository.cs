1ad5f14d8e490fbbc5adcc7c96d772bcd82c3873
Jay <jafolayan06@gmail.com>
2022-02-07 12:09:23 +0100

    Added T< > repository

---
+++ src/eFoodHub.Repositories/Implementations/Repository.cs
@@ -0,0 +1,53 @@
+﻿using eFoodHub.Repositories.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace eFoodHub.Repositories.Implementations
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected DbContext _dbContext;
+
+        public Repository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Add(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+        }
+
+        public void DeleteById(object id)
+        {
+            TEntity entity = _dbContext.Set<TEntity>().Find(id);
+            if (entity is not null)
+                _dbContext.Set<TEntity>().Remove(entity);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _dbContext.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(object id)
+        {
+            return _dbContext.Set<TEntity>().Find(id);
+        }
+
+        public int SaveChanges()
+        {
+            return _dbContext.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+        }
+    }
+}
---
+++ src/eFoodHub.Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace eFoodHub.Repositories.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+
+        TEntity GetById(object id);
+
+        void Add(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        void DeleteById(object id);
+
+        int SaveChanges();
+    }
+}
